33ebf31e8aa960ae9d5b22e2532e1262da29ba39
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-07-18 16:54:50 +0500

    + Base conf for twcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    typography,
+  ],
+}
