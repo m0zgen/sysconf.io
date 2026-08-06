01ce605a034d761668fde985878fb851873ea8e5
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2026-08-06 19:06:02 +0500

    + + Uncle Yevgeniy

--- src/components/2026/Conf2026.vue
+++ src/components/2026/Conf2026.vue
@@ -181,7 +181,7 @@ import MatrixRain from "@/components/reusable/MatrixRainv2.vue";
                 <img class="w-full mx-auto" src="@/assets/images/2026/icons/ufo_head.png" alt="Open SysConf 2026">
               </div>
             </div>
-            <h3 class="text-lg font-semibold mb-1">Uncle Eugene</h3>
+            <h3 class="text-lg font-semibold mb-1">Uncle Yevgeniy</h3>
             <p class="text-xs text-green-400 mb-2">Security DNA <a href="https://openbld.net" target="_blank" class="px-3 py-1 bg-purple-900/30 hover:bg-purple-800/50 text-purple-300 hover:text-green-100 transition">
               OpenBLD.net
             </a></p>
